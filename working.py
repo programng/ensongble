a06cacd5d9615389a2f93b2725dce1fae9a4378b
Jonathan Ng <jon7ng@gmail.com>
2017-03-24 00:56:55 +0000

    update script to add horror films and serialize data more modular

--- working.py
+++ working.py
@@ -33,6 +33,7 @@ import librosa
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import confusion_matrix, precision_score, recall_score
+from sklearn.preprocessing import LabelEncoder
 
 # path to audio file
 # audio_path = "/data/music/batch2/Moana (Original Motion Picture Soundtrack|Deluxe Edition)/04 How Far I'll Go.wav"
@@ -61,7 +62,7 @@ def time_elapsed(start_time):
     current_time()
     print("{} minutes and {} seconds".format(minutes, seconds))
 
-def load_data(genres_list, music_path, sample_number):
+def load_data(genres_list, music_path):
     time_elapsed(start_time)
     """Load audio buffer (data) for all songs for the specified genres
 
@@ -93,13 +94,13 @@ def load_data(genres_list, music_path, sample_number):
         pickle_path = os.path.join('/data/music', pickle_filename)
         # pickle_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), pickle_filename)
         if (os.path.isfile(pickle_path)):
-            print('loading pickled data...')
+            print('loading pickled {} data...'.format(genre))
             time_elapsed(start_time)
             audio_buffer_array_for_genre = np.load(pickle_path)
             # f_r = open(pickle_path)
             # audio_buffer_array_for_genre = cPickle.load(f_r)
             # f_r.close()
-            print('finished loading pickled data')
+            print('finished loading pickled {} data'.format(genre))
             time_elapsed(start_time)
         else:
             print('loading fresh data...')
@@ -113,7 +114,9 @@ def load_data(genres_list, music_path, sample_number):
 
             loaded_audio_for_genre = pool.map(librosa.load, all_songs_for_genre)
             audio_buffer_array_for_genre = np.array([loaded_audio[0] for loaded_audio in loaded_audio_for_genre])
+            print('pickling {} data...'.format(genre))
             np.save(pickle_path, audio_buffer_array_for_genre)
+            print('...finished pickling {} data'.format(genre))
             # f_w = open(pickle_path, 'w')
             # cPickle.dump(audio_buffer_array_for_genre, f_w)
             # f_w.close()
@@ -130,8 +133,10 @@ if __name__ == '__main__':
     start_time = time.time()
     current_time()
 
+
     # user provided list of genres they want to load
-    genres_list = ['family', 'sci-fi']
+    genres_list = ['family', 'horror']
+    # genres_list = ['family', 'sci-fi', 'horror']
     print('genres of interest:')
     for genre in genres_list:
         print('    -', genre)
@@ -140,7 +145,7 @@ if __name__ == '__main__':
     # load dictionary whose keys are genres and values are a list, 0th index is array of songs, 1st index is array of labels
     print('loading audio files...')
 
-    genres_dict = load_data(genres_list, '/data/music', 100)
+    genres_dict = load_data(genres_list, '/data/music')
     time_elapsed(start_time)
 
     print('...finished loading audio files')
@@ -151,53 +156,156 @@ if __name__ == '__main__':
     for genre_data in genres_dict.values():
         Xs.append(genre_data[0])
         ys.append(genre_data[1])
+
     X = np.concatenate(Xs)
     y = np.concatenate(ys)
     print('X', X)
     print('y', y)
 
+###############################
+##### CALCULATE FEATURES #####
+###############################
+
     df = pd.DataFrame(data={'genre': y})
 
-    print('calculating spectral centroids...')
-    spectral_centroids = pool.map(librosa.feature.spectral_centroid, X)
+    print('calculating spectral rolloff...')
     time_elapsed(start_time)
+    pickle_filename = 'spectral_rolloffs_{}.npy'
+    pickle_path_mean = os.path.join('/data/music', pickle_filename.format('mean'))
+    pickle_path_std = os.path.join('/data/music', pickle_filename.format('std'))
+    if (os.path.isfile(pickle_path_mean) and os.path.isfile(pickle_path_std) ):
+        print('loading pickled data...')
+        df['spectral_rolloffs_mean'] = np.load(pickle_path_mean)
+        df['spectral_rolloffs_std'] = np.load(pickle_path_std)
+        print('finished loading pickled data')
+    else:
+        spectral_rolloffs = pool.map(librosa.feature.spectral_rolloff, X)
+        df['spectral_rolloffs_mean'] = [spectral_rolloff.mean() for spectral_rolloff in spectral_rolloffs]
+        df['spectral_rolloffs_std'] = [spectral_rolloff.std() for spectral_rolloff in spectral_rolloffs]
+        np.save(pickle_path_mean, df['spectral_rolloffs_mean'])
+        np.save(pickle_path_std, df['spectral_rolloffs_std'])
+    # spectral_rolloffs = [librosa.feature.spectral_rolloff(x) for x in X]
+    print('...finished calculating spectral rolloff')
+    time_elapsed(start_time)
+
+    print('calculating spectral centroids...')
+    time_elapsed(start_time)
+    pickle_filename = 'spectral_centroids_{}.npy'
+    pickle_path_mean = os.path.join('/data/music', pickle_filename.format('mean'))
+    pickle_path_std = os.path.join('/data/music', pickle_filename.format('std'))
+    if (os.path.isfile(pickle_path_mean) and os.path.isfile(pickle_path_std) ):
+        print('loading pickled data...')
+        df['spectral_centroids_mean'] = np.load(pickle_path_mean)
+        df['spectral_centroids_std'] = np.load(pickle_path_std)
+        print('finished loading pickled data')
+    else:
+        spectral_centroids = pool.map(librosa.feature.spectral_centroid, X)
+        df['spectral_centroids_mean'] = [spectral_centroid.mean() for spectral_centroid in spectral_centroids]
+        df['spectral_centroids_std'] = [spectral_centroid.std() for spectral_centroid in spectral_centroids]
+        np.save(pickle_path_mean, df['spectral_centroids_mean'])
+        np.save(pickle_path_std, df['spectral_centroids_std'])
     # spectral_centroids = [librosa.feature.spectral_centroid(x) for x in X]
-    df['spectral_centroids_mean'] = [spectral_centroid.mean() for spectral_centroid in spectral_centroids]
-    df['spectral_centroids_std'] = [spectral_centroid.std() for spectral_centroid in spectral_centroids]
     print('...finished calculating spectral centroids')
+    time_elapsed(start_time)
+
+    print('calculating zero-crossing rate...')
+    time_elapsed(start_time)
+    pickle_filename = 'zero_crossing_rates_{}.npy'
+    pickle_path_mean = os.path.join('/data/music', pickle_filename.format('mean'))
+    pickle_path_std = os.path.join('/data/music', pickle_filename.format('std'))
+    if (os.path.isfile(pickle_path_mean) and os.path.isfile(pickle_path_std) ):
+        print('loading pickled data...')
+        df['zero_crossing_rates_mean'] = np.load(pickle_path_mean)
+        df['zero_crossing_rates_std'] = np.load(pickle_path_std)
+        print('finished loading pickled data')
+    else:
+        zero_crossing_rates = pool.map(librosa.feature.zero_crossing_rate, X)
+        df['zero_crossing_rates_mean'] = [zero_crossing_rate.mean() for zero_crossing_rate in zero_crossing_rates]
+        df['zero_crossing_rates_std'] = [zero_crossing_rate.std() for zero_crossing_rate in zero_crossing_rates]
+        np.save(pickle_path_mean, df['zero_crossing_rates_mean'])
+        np.save(pickle_path_std, df['zero_crossing_rates_std'])
+    # zero_crossing_rates = [librosa.feature.zero_crossing_rate(x) for x in X]
+    print('...finished calculating zero-crossing rate')
+    time_elapsed(start_time)
 
     print('calculating mfccs...')
-    partial_mfcc = partial(librosa.feature.mfcc, n_mfcc=5)
-    mfccs = pool.map(partial_mfcc, X)
     time_elapsed(start_time)
-    # mfccs = [librosa.feature.mfcc(x, n_mfcc=5) for x in X]
+
+    pickle_filename = 'mfccs_{number}_{metric}.npy'
+    pickle_path_mean_1 = os.path.join('/data/music', pickle_filename.format(number=1, metric='mean'))
+    pickle_path_std_1 = os.path.join('/data/music', pickle_filename.format(number=1, metric='std'))
+    pickle_path_mean_2 = os.path.join('/data/music', pickle_filename.format(number=2, metric='mean'))
+    pickle_path_std_2 = os.path.join('/data/music', pickle_filename.format(number=2, metric='std'))
+    pickle_path_mean_3 = os.path.join('/data/music', pickle_filename.format(number=3, metric='mean'))
+    pickle_path_std_3 = os.path.join('/data/music', pickle_filename.format(number=3, metric='std'))
+    pickle_path_mean_4 = os.path.join('/data/music', pickle_filename.format(number=4, metric='mean'))
+    pickle_path_std_4 = os.path.join('/data/music', pickle_filename.format(number=4, metric='std'))
+    pickle_path_mean_5 = os.path.join('/data/music', pickle_filename.format(number=5, metric='mean'))
+    pickle_path_std_5 = os.path.join('/data/music', pickle_filename.format(number=5, metric='std'))
+
+    if (os.path.isfile(pickle_path_mean_1) and os.path.isfile(pickle_path_std_1) ):
+        print('loading pickled data...')
+        df['mfcc1_mean'] = np.load(pickle_path_mean_1)
+        df['mfcc1_std'] = np.load(pickle_path_mean_1)
+        df['mfcc2_mean'] = np.load(pickle_path_mean_2)
+        df['mfcc2_std'] = np.load(pickle_path_mean_2)
+        df['mfcc3_mean'] = np.load(pickle_path_mean_3)
+        df['mfcc3_std'] = np.load(pickle_path_mean_3)
+        df['mfcc4_mean'] = np.load(pickle_path_mean_4)
+        df['mfcc4_std'] = np.load(pickle_path_mean_4)
+        df['mfcc5_mean'] = np.load(pickle_path_mean_5)
+        df['mfcc5_std'] = np.load(pickle_path_mean_5)
+        print('finished loading pickled data')
+    else:
+        partial_mfcc = partial(librosa.feature.mfcc, n_mfcc=5)
+        mfccs = pool.map(partial_mfcc, X)
+        # mfccs = [librosa.feature.mfcc(x, n_mfcc=5) for x in X]
+        print('...finished calculating mfccs')
+        print('calculating mfcc1...')
+        mfcc1s = [mfcc[0] for mfcc in mfccs]
+        df['mfcc1_mean'] = [mfcc1.mean() for mfcc1 in mfcc1s]
+        df['mfcc1_std'] = [mfcc1.std() for mfcc1 in mfcc1s]
+        print('...finished calculating mfcc1')
+        print('calculating mfcc2...')
+        mfcc2s = [mfcc[1] for mfcc in mfccs]
+        df['mfcc2_mean'] = [mfcc2.mean() for mfcc2 in mfcc2s]
+        df['mfcc2_std'] = [mfcc2.std() for mfcc2 in mfcc2s]
+        print('...finished calculating mfcc2')
+        print('calculating mfcc3...')
+        mfcc3s = [mfcc[2] for mfcc in mfccs]
+        df['mfcc3_mean'] = [mfcc3.mean() for mfcc3 in mfcc3s]
+        df['mfcc3_std'] = [mfcc3.std() for mfcc3 in mfcc3s]
+        print('...finished calculating mfcc3')
+        print('calculating mfcc4...')
+        mfcc4s = [mfcc[3] for mfcc in mfccs]
+        df['mfcc4_mean'] = [mfcc4.mean() for mfcc4 in mfcc4s]
+        df['mfcc4_std'] = [mfcc4.std() for mfcc4 in mfcc4s]
+        print('...finished calculating mfcc4')
+        print('calculating mfcc5...')
+        mfcc5s = [mfcc[4] for mfcc in mfccs]
+        df['mfcc5_mean'] = [mfcc5.mean() for mfcc5 in mfcc5s]
+        df['mfcc5_std'] = [mfcc5.std() for mfcc5 in mfcc5s]
+        print('...finished calculating mfcc5')
+
+        np.save(pickle_path_mean_1, df['mfcc1_mean'])
+        np.save(pickle_path_std_1, df['mfcc1_std'])
+        np.save(pickle_path_mean_2, df['mfcc2_mean'])
+        np.save(pickle_path_std_2, df['mfcc2_std'])
+        np.save(pickle_path_mean_3, df['mfcc3_mean'])
+        np.save(pickle_path_std_3, df['mfcc3_std'])
+        np.save(pickle_path_mean_4, df['mfcc4_mean'])
+        np.save(pickle_path_std_4, df['mfcc4_std'])
+        np.save(pickle_path_mean_5, df['mfcc5_mean'])
+        np.save(pickle_path_std_5, df['mfcc5_std'])
+
     print('...finished calculating mfccs')
-    print('calculating mfcc1...')
-    mfcc1s = [mfcc[0] for mfcc in mfccs]
-    df['mfcc1_mean'] = [mfcc1.mean() for mfcc1 in mfcc1s]
-    df['mfcc1_std'] = [mfcc1.std() for mfcc1 in mfcc1s]
-    print('...finished calculating mfcc1')
-    print('calculating mfcc2...')
-    mfcc2s = [mfcc[1] for mfcc in mfccs]
-    df['mfcc2_mean'] = [mfcc2.mean() for mfcc2 in mfcc2s]
-    df['mfcc2_std'] = [mfcc2.std() for mfcc2 in mfcc2s]
-    print('...finished calculating mfcc2')
-    print('calculating mfcc3...')
-    mfcc3s = [mfcc[2] for mfcc in mfccs]
-    df['mfcc3_mean'] = [mfcc3.mean() for mfcc3 in mfcc3s]
-    df['mfcc3_std'] = [mfcc3.std() for mfcc3 in mfcc3s]
-    print('...finished calculating mfcc3')
-    print('calculating mfcc4...')
-    mfcc4s = [mfcc[3] for mfcc in mfccs]
-    df['mfcc4_mean'] = [mfcc4.mean() for mfcc4 in mfcc4s]
-    df['mfcc4_std'] = [mfcc4.std() for mfcc4 in mfcc4s]
-    print('...finished calculating mfcc4')
-    print('calculating mfcc5...')
-    mfcc5s = [mfcc[4] for mfcc in mfccs]
-    df['mfcc5_mean'] = [mfcc5.mean() for mfcc5 in mfcc5s]
-    df['mfcc5_std'] = [mfcc5.std() for mfcc5 in mfcc5s]
-    print('...finished calculating mfcc5')
     time_elapsed(start_time)
+
+
+
+########################################
+##### SHUFFLE DATA AND TRAIN | TEST #####
+########################################
 
     # shuffle rows for cross-validation
     df = df.sample(frac=1)
@@ -229,15 +337,21 @@ if __name__ == '__main__':
     print(confusion_matrix(y_test, y_predict))
 
     # get precision
-    # print("precision:", precision_score(y_test, y_predict))
+    le = LabelEncoder()
+    le.fit(y_train)
+    le_y_test = le.transform(y_test)
+    le_y_predict = le.transform(y_predict)
+    print("precision:", precision_score(le_y_test, le_y_predict))
 
     # get recall
-    # print("recall:", recall_score(y_test, y_predict))
+    print("recall:", recall_score(le_y_test, le_y_predict))
 
     print('starting cross validation, k=5...')
     scores = cross_val_score(rf, X, y, cv=5, n_jobs=-1)
     print('...finished cross validation')
     print('cross validation scores:', scores)
+    print('average of cross validation scores:', scores.mean())
+    time_elapsed(start_time)
 
 
 
